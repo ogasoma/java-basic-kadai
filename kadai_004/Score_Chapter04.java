275babe71d7c77fff30d5aca5dbf505c89d41395
ogasoma <154291837+ogasoma@users.noreply.github.com>
2024-02-02 10:49:18 +0900

    Add files via upload

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,33 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		int d = 40;
+		int e = 50;
+		int f = 60;
+		int g = 70;
+		int h = 80;
+		int i = 90;
+		int j = 100;
+		int average = (a+b+c+d+e+f+g+h+i+j)/10;
+
+	      System.out.println("Aさんの点数は" + a + "です"); 
+	      System.out.println("Aさんの点数は" + b + "です"); 
+	      System.out.println("Aさんの点数は" + c + "です"); 
+	      System.out.println("Aさんの点数は" + d + "です"); 
+	      System.out.println("Aさんの点数は" + e + "です"); 
+	      System.out.println("Aさんの点数は" + f + "です"); 
+	      System.out.println("Aさんの点数は" + g + "です"); 
+	      System.out.println("Aさんの点数は" + h + "です"); 
+	      System.out.println("Aさんの点数は" + i + "です"); 
+	      System.out.println("Aさんの点数は" + average + "です"); 
+
+	      
+	        
+	}
+
+}
