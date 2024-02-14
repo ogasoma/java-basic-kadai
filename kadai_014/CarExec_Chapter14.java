e2d4b13516d2de44920b0fd2822fb90bfaba633a
ogasoma <154291837+ogasoma@users.noreply.github.com>
2024-02-15 08:46:02 +0900

    Add files via upload

--- kadai_014/CarExec_Chapter14.java
+++ kadai_014/CarExec_Chapter14.java
@@ -1,13 +1,10 @@
 package kadai_014;
-
 public class CarExec_Chapter14 {
-
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-		
-		
-		
-	}
 
+	Car_chapter14 car1 = new Car_chapter14(1,10);
+
+	car1.gearChange(3);
+	car1.run();
+	}
 }
--- kadai_014/Car_chapter14.java
+++ kadai_014/Car_chapter14.java
@@ -1,44 +1,33 @@
 package kadai_014;
 
+public class Car_chapter14 {
+	private	int	gear ;	//ギア
+	private	int	speed ;	//スピード
 
-		
-		public class gearChange {
-		    private int    gear = 1;  // 価格（税込）
-		    private int    speed = 10;  // 在庫数
-		    
-		}
-		    
-
-
-	    // 【メソッド】注文を受けて在庫数を更新する
-	    // number：注文数
-	    public void gearChange( final int afterGear ) {
-
-	        System.out.println("ギア" + this.gear + "から" + this.speed + "に切り替えました");
-
-
-	        // 注文数が在庫数を超えていたら購入不可
-	        if( this.speed < number ) {
-	            System.out.println( this.name + "は在庫不足です" );
-	            return; // 購入できないため終了
-	        }
-
-	        
-	        
-	        // 変数i＝1～10の範囲で繰り返す
-	        for( int i = 1; i == gear; i++ ) {
-
-	            // ループカウンタが3のときに繰り返し処理をスキップ
-	            if( i == gear ) {
-	                System.out.println("次の繰り返し処理に進みます");
-	                continue;
-	            }
-
-	            System.out.println(i);
-	        }
-	        
-	        
-	   
+	Car_chapter14(int gear ,int speed){
+		this. gear = gear;
+		this. speed = speed;
 	}
-	    }
+	
+	public void gearChange(int afterGear) {
+		System.out.println("ギア"+gear+"からギア"+afterGear+"に変わりました");
+		speed = afterGear;
+	}
+
+	public void run() {
+		switch( speed) {
+        case 1 -> System.out.println("速度は時速10キロです");
+        case 2 -> System.out.println("速度は時速20キロです");
+        case 3 -> System.out.println("速度は時速30キロです");
+        case 4 -> System.out.println("速度は時速40キロです");
+        case 5 -> System.out.println("速度は時速50キロです");
+      default   -> System.out.println("速度は時速10キロです");
+	}
+	}
+}
+
+
+
+
+
 	
