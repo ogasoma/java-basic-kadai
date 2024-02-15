56ee245fb7acdd9c23e54fdb607ee6d4f110aea9
ogasoma <154291837+ogasoma@users.noreply.github.com>
2024-02-15 22:19:29 +0900

    Update Car_chapter14.java

--- kadai_014/Car_chapter14.java
+++ kadai_014/Car_chapter14.java
@@ -1,33 +1,24 @@
 package kadai_014;
 
 public class Car_chapter14 {
-	private	int	gear ;	//ギア
-	private	int	speed ;	//スピード
+	private	int	gear = 1;	//ギア
+	private	int	speed = 10;	//スピード
 
-	Car_chapter14(int gear ,int speed){
-		this. gear = gear;
-		this. speed = speed;
-	}
 	
 	public void gearChange(int afterGear) {
-		System.out.println("ギア"+gear+"からギア"+afterGear+"に変わりました");
-		speed = afterGear;
+		 gear = afterGear;
+		 speed = switch (gear) { 
+		 	case 1 -> 10;
+		 	case 2 -> 20;
+		 	case 3 -> 30;
+		 	case 4 -> 40;
+		 	default -> 10; 
+		 };
+	}
+	public void run() {
+		System.out.println("ギア1からギア"+gear+"に変わりました");
+		System.out.println("速度は" + speed + "kmです");
 	}
 
-	public void run() {
-		switch( speed) {
-        case 1 -> System.out.println("速度は時速10キロです");
-        case 2 -> System.out.println("速度は時速20キロです");
-        case 3 -> System.out.println("速度は時速30キロです");
-        case 4 -> System.out.println("速度は時速40キロです");
-        case 5 -> System.out.println("速度は時速50キロです");
-      default   -> System.out.println("速度は時速10キロです");
-	}
-	}
 }
 
-
-
-
-
-	
